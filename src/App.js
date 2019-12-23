179f2eb91e359dec868d3998de5723ad2c536b9b
Carvajal Suazo <jcarvajals@deloitte.com>
2019-12-23 11:07:38 -0300

    Agregado travis file

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reeeeact
+          Learn React
         </a>
       </header>
     </div>
